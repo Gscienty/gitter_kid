7cf12bd7abc03dcc6d1a61eb313ce7daf767f638
Gscienty <gaoxiaochuan@hotmail.com>
2017-12-28 17:57:28 +0800

    try wrapper Market

---
+++ repo/src/test/java/indi/gscienty/gitterkid/repo/MarketTest.java
@@ -0,0 +1,38 @@
+package indi.gscienty.gitterkid.repo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class MarketTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public MarketTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( MarketTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+
+    }
+}

49cf70221c0d9d320c2bcfa2be996b2af054bfde
Gscienty <gaoxiaochuan@hotmail.com>
2018-01-09 14:07:02 +0800

    wrap ref_delta

--- native/src/gitfs_obj_ref_delta.c
+++ native/src/gitfs_obj_ref_delta.c
@@ -2,40 +2,48 @@
 #include <string.h>
 #include <malloc.h>
 
-struct git_obj *__git_packitem_transfer_ref_delta (struct __git_packitem *packitem, const char *signture) {
+struct git_obj *__git_packitem_transfer_ref_delta_build_git_obj (struct __git_packitem *packitem, const char *signture) {
     struct git_obj *ret = (struct git_obj *) malloc (sizeof (*ret));
     if (ret == NULL) {
         DBG_LOG (DBG_ERROR, "__git_packitem_transfer_ref_delta: have not enough free memory");
         return NULL;
     }
+    struct __obj_file_ret *inflated_obj = __git_packitem_inflate (packitem);
+    ret->buf = inflated_obj->buf;
+    ret->path = NULL;
+    ret->sign = strdup (signture);
+    ret->type = GIT_OBJ_TYPE_REF_DELTA;
+    ret->ptr = ref_delta;
+    ret->size = inflated_obj->len;
+    ret->body = inflated_obj->buf;
+    free (inflated_obj);
+
+    return ret;
+}
+
+struct git_obj *__git_packitem_transfer_ref_delta (struct __git_packitem *packitem, const char *signture) {
     struct git_obj_ref_delta *ref_delta = (struct git_obj_ref_delta *) malloc (sizeof (*ref_delta));
     if (ref_delta == NULL) {
         DBG_LOG (DBG_ERROR, "__git_packitem_transfer_ref_delta: have not enough free memory");
-        free (ret);
         return NULL;
     }
+    // get base sign
     ref_delta->base_sign = (void *) malloc (20);
     if (ref_delta->base_sign == NULL) {
         DBG_LOG (DBG_ERROR, "__git_packitem_transfer_ref_delta: have not enough free memory");
-        free (ret);
         free (ref_delta);
         return NULL;
     }
     memcpy (ref_delta->base_sign, packitem->mmaped_base + packitem->inner_offset, 20);
     packitem->inner_offset += 20;
 
-    struct __obj_file_ret *inflated_obj = __git_packitem_inflate (packitem);
-    ref_delta->content = inflated_obj->buf;
-    ref_delta->len = inflated_obj->len;
-    free (inflated_obj);
+    // wrapped to git_obj
+    struct git_obj *ret = __git_packitem_transfer_ref_delta_build_git_obj (packitem, signture);
+    
+    ref_delta->head = ref_delta->tail = ref_delta->cursor = NULL;
+    __git_obj_transfer_delta (ret, &ref_delta->head, &ref_delta->tail);
 
-    ret->buf = ref_delta->content;
-    ret->path = NULL;
-    ret->sign = strdup (signture);
-    ret->type = GIT_OBJ_TYPE_REF_DELTA;
     ret->ptr = ref_delta;
-    ret->size = ref_delta->len;
-    ret->body = ref_delta->content;
 
     return ret;
 }

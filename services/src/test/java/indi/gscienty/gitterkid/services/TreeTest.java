7e098fb9e98a87db5acc875bb70152024d774e7b
Gscienty <gaoxiaochuan@hotmail.com>
2018-01-26 16:02:23 +0800

    try test dtor error

--- services/src/test/java/indi/gscienty/gitterkid/services/TreeTest.java
+++ services/src/test/java/indi/gscienty/gitterkid/services/TreeTest.java
@@ -1,25 +1,29 @@
 package indi.gscienty.gitterkid.services;
 
+import indi.gscienty.gitterkid.repo.GitTree;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class CommitTest extends TestCase {
-	public CommitTest(String name) {
-		super (name);
+public class TreeTest extends TestCase {
+	public TreeTest(String name) {
+		super(name);
 	}
 	
 	public static Test suite() {
-		return new TestSuite(CommitTest.class);
+		return new TestSuite(TreeTest.class);
 	}
 	
-	public void testGetSubTree() {
+	public void testTree() {
 		GitRepositoriesService reposService = new GitRepositoriesService("/home/ant");
 		GitBranchesService bransService = new GitBranchesService(reposService.getRepository("gitterRepo"));
 		
 		
 		GitCommitService commService = new GitCommitService(bransService.getLastCommit("master"));
+
+		GitTree tree = commService.getTree("/services/src/main/java/indi/gscienty/gitterkid/services/");
 		
-		assertNotNull(commService);
+		tree.forEach(i -> System.out.println(i.getName()));
+		
 	}
 }

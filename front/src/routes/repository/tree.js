0b0852bc780c247f92db8801683c8abd82ecf9dc
高小川 <gaoxiaochuan@hotmail.com>
2018-01-29 19:50:47 +0800

    comp tree

--- front/src/routes/repository/tree.js
+++ front/src/routes/repository/tree.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import ProjectTemplate from './_project';
-import { Table, Icon } from 'antd';
+import { Table, Icon, Card } from 'antd';
 import { connect } from 'react-redux';
 
 class Page extends ProjectTemplate {
     state = { path: '', prefixURI: '' }
 
-    freshTree(path) {
-        if (path === undefined) {
-            path = '';
-            let prePaths = this.props.match.url.split('/tree');
-            if (prePaths.length === 2) {
-                path += prePaths[1];
-            }
-            else {
-                path = '/'
-            }
+    entryTreeInitialize() {
+        let pathItems = window.location.hash.substring(1).split('/tree');
+        let path = '/';
+        if (pathItems.length === 2) {
+            path = pathItems[1];
+        }
 
-            this.setState({ path, prefixURI: prePaths[0] });
+        if (path !== this.state.path) {
+            this.setState({ path }, () => this.freshTree(path));
         }
-        else { 
-            this.setState({ path });
-        }
+    }
+
+    componentDidMount() {
+        this.entryTreeInitialize();
+    }
+
+    componentWillReceiveProps(props) {
+        this.entryTreeInitialize();
+    }
+
+    freshTree(path) {
         this.props.dispatch({
             type: 'tree/getTree',
             payload: {
@@ -33,19 +38,37 @@ class Page extends ProjectTemplate {
         });
     }
 
-    componentDidMount() {
-        this.freshTree();
-    }
-
-    componentWillReceiveProps(props) {
-        this.freshTree();
-    }
-
     treeItemOnClick(record) {
         if (record.type === 'Tree') {
-            this.props.history.push(this.state.prefixURI + '/tree' + this.state.path + record.name + '/');
+            this.props.history.push(this.state.prefixURI + this.state.path + record.name + '/');
             this.freshTree(this.state.path + record.name + '/');
         }
+    }
+
+    renderPath() {
+        let pathItems = this.state.path.substring(0, this.state.path.length - 1).split('/');
+        pathItems[0] = {
+            name: '',
+            path: ''
+        }
+        for (let i = 1; i < pathItems.length; i++) {
+            pathItems[i] = {
+                name: pathItems[i],
+                path: pathItems[i - 1].path + '/' + pathItems[i]
+            };
+        }
+
+        pathItems[0].name = this.props.match.params.repositoryName;
+        pathItems[0].path = '';
+
+        return pathItems.map((item, index) => <span style={{ fontSize: 21 }}>
+            { index === 0 ? '' : '/' }
+            <a onClick={() => {
+                this.props.history.push(this.state.prefixURI + item.path + '/');
+                this.freshTree(item.path + '/');
+            }}
+            style={{ margin: '0 4px' }}>{ item.name }</a>
+        </span>);
     }
 
     columns = [
@@ -59,15 +82,17 @@ class Page extends ProjectTemplate {
     ]
 
     unit() {
-        return <div>
-            <span>{ this.state.path }</span>
+        
+        return <Card
+            title={ <span>{ this.renderPath() }</span> }
+            bodyStyle={{ padding: 0 }}>
             <Table
                 columns={ this.columns }
                 dataSource={ this.props.tree.content.map(e => ({ ...e, key: e.name })) }
                 showHeader={ false }
                 pagination={ false }
             />
-        </div>;
+        </Card>;
     }
 }
 

a0c3841581ed2e248d474707d07ec9e8d2730ea3
高小川 <gaoxiaochuan@hotmail.com>
2018-04-04 10:19:43 +0800

    add repository interface

---
+++ native-gitfsi/include/repository.h
@@ -0,0 +1,15 @@
+#ifndef _GIT_FSI_REPOSITORY_
+#define _GIT_FSI_REPOSITORY_
+
+#include <string>
+
+namespace gitterKid {
+    namespace fsi {
+        class repository {
+            public:
+                
+        };
+    }
+}
+
+#endif

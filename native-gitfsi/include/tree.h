d39b6c2d9ef42752a2fda31319e4dd5218f07f98
高小川 <gaoxiaochuan@hotmail.com>
2018-08-24 16:22:36 +0800

    Update tree.h

--- native-gitfsi/include/tree.h
+++ native-gitfsi/include/tree.h
@@ -6,32 +6,34 @@
 #include <vector>
 #include <string>
 
-namespace gitterKid {
-    namespace fsi {
-        
-        class treeItem {
-            private:
-                std::string sign;
-                std::string name;
-                objectType type;
-            public:
-                treeItem(std::string sign, std::string name, objectType type)
-                    : sign(sign), name(name), type(type) { }
-                std::string getSign() const { return this->sign; }
-                std::string getName() const { return this->name; }
-                objectType getType() const { return this->type; }
-        };
+namespace gitter_kid {
+namespace fsi {
 
-        class tree : public content {
-            private:
-                std::vector<treeItem> &items;
-            public:
-                objectType getType() const { return objectType::treeType; }
-                tree(std::vector<treeItem> &items, std::vector<byte>::iterator spliter, std::vector<byte>::iterator end);
-                tree(const content &origin);
-                std::vector<treeItem> get() const { return this->items; }
-        };
+class tree_item {
+private:
+    std::string _sign;
+    std::string _name;
+    gitter_kid::fsi::obj_type _type;
+public:
+    treeItem(std::string sign, std::string name, gitter_kid::fsi::obj_type type)
+        : _sign(sign), _name(name), _type(type) { }
+    std::string &sign() { return this->_sign; }
+    std::string &name() const { return this->_name; }
+    gitter_kid::fsi::obj_type &type() { return this->_type; }
+};
+
+class tree : public gitter_kid::fsi::content {
+private:
+    std::vector<gitter_kid::fsi::tree_item> _items;
+public:
+    virtual gitter_kid::fsi::obj_type type() const override {
+        return gitter_kid::fsi::obj_type::obj_type_tree;
     }
+    tree(std::basic_string<byte>::iterator spliter, std::basic_string<byte>::iterator end);
+    std::vector<gitter_kid::fsi::tree_item> &get() { return this->_items; }
+};
+
+}
 }
 
 #endif

66f05dcc0a829ac896868f3a62eb415bd65aea66
高小川 <gaoxiaochuan@hotmail.com>
2018-08-24 15:56:05 +0800

    Update object.h

--- native-gitfsi/include/object.h
+++ native-gitfsi/include/object.h
@@ -7,29 +7,32 @@
 #include <string>
 #include <vector>
 
-namespace gitterKid {
-    namespace fsi {
+namespace gitter_kid {
+namespace fsi {
 
-        class object {
-            private:
-                objectType analysisType(std::vector<byte>& store, std::vector<byte>::iterator& spliter);
-            protected:
-                const repository& repo;
-                const std::string signture;
-                objectType type;
-                void *bodyBuffer; //lazy
-                content *body;
+class object {
+    private:
+        gitter_kid::fsi::obj_type
+        analysis_type(std::basic_string<byte>& store,
+                     std::basic_string<byte>::iterator& spliter);
+    protected:
+        const gitter_kid::fsi::repository& _repo;
+        const std::string _sign;
+        gitter_kid::fsi::obj_type type;
+        void *_body_buffer; //lazy
+        gitter_kid::fsi::content *_body;
 
-                virtual std::vector<byte> getStore() = 0;
-            public:
-                object(const repository& repo, std::string signture);
-                ~object();
-                void initialize();
-                objectType getType() const;
+        virtual std::basic_string<byte> store() = 0;
+    public:
+        object(const gitter_kid::fsi::repository& repo, std::string signture);
+        ~object();
+        void initialize();
+        gitter_kid::fsi::obj_type type() const;
 
-                template<class T> T get() const { return static_cast<T>(*this->body); }
-        };
-    }
+        template<class T> T get() const { return dynamic_cast<T>(*this->_body); }
+};
+
+}
 }
 
 #endif

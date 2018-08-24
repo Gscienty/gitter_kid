9f365f543effe22ea19a547b6f23bad74930a4b6
高小川 <gaoxiaochuan@hotmail.com>
2018-08-24 14:50:25 +0800

    Update content.h

--- native-gitfsi/include/content.h
+++ native-gitfsi/include/content.h
@@ -8,7 +8,15 @@
 namespace gitterKid {
 namespace fsi {
 
-enum obj_type { obj_type_unknow, obj_type_blob, obj_type_commit, obj_type_tree, obj_type_tag, obj_type_ofsdelta, obj_type_refdelta };
+enum obj_type {
+    obj_type_unknow,
+    obj_type_blob,
+    obj_type_commit,
+    obj_type_tree,
+    obj_type_tag,
+    obj_type_ofsdelta,
+    obj_type_refdelta
+};
 
 class content {
 protected:

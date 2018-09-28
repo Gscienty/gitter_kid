8c22ac3d14ee172133006e5bbce7498e63ac5f4a
高小川 <gaoxiaochuan@hotmail.com>
2018-09-28 19:50:23 +0800

    add http_buf's get methods

--- http_server/include/http_buf.h
+++ http_server/include/http_buf.h
@@ -8,18 +8,37 @@
 
 namespace http_server {
 
+typedef uint8_t http_buf_direction_t;
+
+const http_buf_direction_t in_buf = 0;
+const http_buf_direction_t out_buf = 1;
+
 class http_buf : public std::basic_streambuf<uint8_t> {
 private:
     int _fd;
+    http_server::http_buf_direction_t _direction;
     size_t _page_size;
     uint8_t *_activity_page;
+
+    int __out_sync();
+    int __in_sync();
+
 protected:
-    virtual http_buf* setbuf(char_type* s, std::streamsize n) override;
     virtual int sync() override;
-    virtual int_type overflow(int_type c) override;
-    virtual std::streamsize xsputn(const char_type* s, std::streamsize n) override;
+    virtual http_buf* setbuf(char_type* s, std::streamsize n) override;
+
+    // put
+    virtual http_server::http_buf::int_type overflow(int_type c) override;
+    virtual std::streamsize xsputn(const http_server::http_buf::char_type* s,
+                                   std::streamsize n) override;
+
+
+    // get
+    virtual http_server::http_buf::int_type underflow() override;
+    virtual std::streamsize xsgetn(http_server::http_buf::char_type* s,
+                                   std::streamsize n) override;
 public:
-    http_buf(int fd);
+    http_buf(int fd, http_buf_direction_t direction);
 };
 
 }
--- http_server/src/http_buf.cc
+++ http_server/src/http_buf.cc
@@ -5,8 +5,9 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-http_server::http_buf::http_buf(int fd)
+http_server::http_buf::http_buf(int fd, http_server::http_buf_direction_t direction)
     : _fd(fd)
+    , _direction(direction)
     , _page_size(0)
     , _activity_page(nullptr) {}
 
@@ -14,23 +15,65 @@ http_server::http_buf* http_server::http_buf::setbuf(http_server::http_buf::char
     this->_activity_page = s;
     this->_page_size = n;
 
-    this->setp(s, s + n);
+    switch (this->_direction) {
+    case http_server::out_buf:
+        this->setp(s, s + n);
+        break;
+
+    case http_server::in_buf:
+        this->setg(s, s, s);
+        break;
+    }
     return this;
 }
 
-int http_server::http_buf::sync() {
+int http_server::http_buf::__out_sync() {
     if (this->pptr() == this->pbase()) {
         return 0;
     }
+
     ::write(this->_fd, this->_activity_page, this->pptr() - this->pbase());
+
+    this->setp(this->_activity_page, this->_activity_page + this->_page_size);
+
     return 0;
 }
 
-std::basic_streambuf<uint8_t>::int_type
+int http_server::http_buf::__in_sync() {
+    size_t remain_unread_size = this->egptr() - this->gptr();
+    if (remain_unread_size != 0) {
+        std::copy(this->gptr(), this->egptr(), this->_activity_page);
+    }
+
+    size_t newly_bytes_size = ::read(this->_fd,
+                                     this->_activity_page + remain_unread_size,
+                                     this->_page_size - remain_unread_size);
+
+    this->setg(this->_activity_page,
+               this->_activity_page,
+               this->_activity_page + remain_unread_size + newly_bytes_size);
+
+    return 0;
+}
+
+int http_server::http_buf::sync() {
+
+    switch (this->_direction) {
+    case http_server::out_buf:
+        return this->__out_sync();
+    case http_server::in_buf:
+        return this->__in_sync();
+    default:
+        return -1;
+    }
+
+}
+
+http_server::http_buf::int_type
 http_server::http_buf::
 overflow(http_server::http_buf::int_type c) {
     this->sync();
-    this->setp(this->_activity_page, this->_activity_page + this->_page_size);
+
     this->_activity_page[0] = uint8_t(c);
     this->pbump(1);
 
@@ -73,3 +116,39 @@ xsputn(const http_server::http_buf::char_type* s, std::streamsize n) {
     return n - remain_puts_bytes;
 }
 
+http_server::http_buf::int_type http_server::http_buf::underflow() {
+    this->sync();
+
+    if (this->gptr() == this->egptr()) {
+        return traits_type::eof();
+    }
+
+    return this->_activity_page[0];
+}
+
+std::streamsize 
+http_server::http_buf::
+xsgetn(http_server::http_buf::char_type* s, std::streamsize n) {
+    size_t remain_gets_bytes = n;
+    http_server::http_buf::char_type* s_pos = s; 
+    
+    if (this->gptr() == this->egptr()) {
+        this->underflow();
+    }
+
+    while (remain_gets_bytes != 0) {
+        size_t buffer_remain_bytes = this->egptr() - this->gptr() ;
+        if (buffer_remain_bytes <= 0) {
+            this->underflow();
+        }
+
+        size_t read_count = std::min(buffer_remain_bytes, remain_gets_bytes);
+
+        std::copy(this->gptr(), this->gptr() + read_count, s_pos);
+        s_pos += read_count;
+        remain_gets_bytes -= read_count;
+        this->gbump(read_count);
+    }
+
+    return n - remain_gets_bytes;
+}

dd6d3986de1cb27fc72e3c655c5a1c5071944b19
高小川 <gaoxiaochuan@hotmail.com>
2018-09-28 19:51:27 +0800

    add http response base class

--- http_server/include/response.h
+++ http_server/include/response.h
@@ -1,25 +1,34 @@
 #pragma once
 
-#include "basic_omsg.h"
+#include "http_buf.h"
 #include "status_code.h"
 #include "version.h"
 #include <map>
 #include <string>
+#include <ios>
 
 namespace http_server {
 
-class response : public http_server::basic_omsg {
+class response : private std::basic_ios<uint8_t> {
 private:
+    int _fd;
+    http_server::http_buf _http_buf;
+
     http_server::version_t _version;
     http_server::status_code_t _status_code;
     std::map<std::string, std::string> _header_parameters;
-protected:
-    virtual void write_metadata() override;
+    std::basic_string<uint8_t> _data;
+
+    void crlf();
 public:
     response(int fd);
+    void set_cache(const http_server::response::char_type* s, size_t n);
     http_server::version_t& version();
     http_server::status_code_t& status_code();
     std::map<std::string, std::string>& header_parameters();
+    void write(const http_server::response::char_type* s, std::streamsize n);
+    void write_metadata();
+    void flush();
 };
 
 }

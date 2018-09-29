9c48c7ccf802aa6c4541d6ea62092649582d5aa0
高小川 <gaoxiaochuan@hotmail.com>
2018-09-30 00:50:18 +0800

    add 404

--- http_server/include/status_code.h
+++ http_server/include/status_code.h
@@ -9,9 +9,11 @@ namespace http_server {
 typedef uint16_t status_code_t;
 
 const status_code_t status_code_ok = 200;
+const status_code_t status_code_notfound = 404;
 
 const std::map<status_code_t, std::string> status_codes = {
-    { 200, "OK" }
+    { 200, "OK" },
+    { 404, "Not Found" }
 };
 
 }
